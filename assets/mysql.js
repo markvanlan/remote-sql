d1528172cb84b92820b0f9c0b87e6bcbc22874bb
Mark VanLandingham <markvanlan@gmail.com>
2018-07-09 16:26:36 -0500

    sql data now populating tabes

---
+++ assets/mysql.js
@@ -0,0 +1,12 @@
+$(function() {
+  $.get('/mysql-databases', function(data) {
+    for(var i=0;i<data.length;i++){
+      $('#databases').append(databaseTemplate(data[i]))
+    }
+  })
+
+  let databaseTemplate = function(name){
+    return ("<a href='/mysql/" + name + "''> <li class='database'>" + name + "</li> </a>")
+  }
+
+});
---
+++ assets/mysql_tables.js
@@ -0,0 +1,39 @@
+let getDataForTable = function(table) {
+  $('#data').empty()
+ var pageURL = window.location.href;
+  var db = pageURL.substr(pageURL.lastIndexOf('/') + 1);
+  url = "/mysql/"+db+"/table/"+table
+  $.get(url, function(data){
+    createTableFromData(data[0], data[1])
+    console.log(data)
+  })
+}
+
+function createTableFromData(details, data) {
+  $('#data').append(createHeaderRow(details))
+  $('#data').append(createDataRows(data))
+  console.log(createDataRows(data))
+}
+
+function createHeaderRow(details) {
+  let html = "<tr id='data-headers'>"
+  for(let i=0;i<details.length;i++) {
+    html = html + "<th>" + details[i].Field + "</th>"
+  }
+  return(html = html + "</tr>")
+}
+
+function createDataRows(data) {
+  let html = ""
+  for(let i=0;i<data.length;i++) {
+    html = html + "<tr>"
+    for (var key in data[i]) {
+      html = html + "<td>" + data[i][key] + "</td>"
+    }
+    for(let y=0;y<data[i].length;y++){
+      
+    }
+    html = html + "</tr>"
+  }
+  return(html)
+}
