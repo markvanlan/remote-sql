248f1b156f2698e475280b81b93ad533eaae8202
Mark VanLandingham <markvanlan@gmail.com>
2020-02-18 16:32:59 -0600

    Return to index with invalid creds

--- assets/index.js
+++ assets/index.js
@@ -7,16 +7,30 @@ $( document ).ready(function() {
   $( "#postgres-selector" ).click(function() {
     toggleToPostgres()
   });
+
+  if (getUrlParameter('type') === "postgres") {
+    toggleToPostgres()
+  }
+
 });
 
 function toggleToPostgres() {
   $('#postgres').show()
   $('#mysql').hide()
-  $('body').css('background','linear-gradient(90deg,#bfd3ff,#5578da)')
+  document.body.classList.remove("bg-red-gradient")
+  document.body.classList.add("bg-blue-gradient")
 }
 
 function toggleToMysql() {
   $('#postgres').hide()
   $('#mysql').show()
-  $('body').css('background','linear-gradient(90deg,#ffbfbf,#da5555)')
+  document.body.classList.remove("bg-blue-gradient")
+  document.body.classList.add("bg-red-gradient")
 }
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
